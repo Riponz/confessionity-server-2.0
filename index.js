6eea42591259196ecb3c764990384fb094735894
Diganta Biswas <51294318+Riponz@users.noreply.github.com>
2024-05-06 01:44:16 +0530

    initial bolte

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const userRoute = require('./routes/userRoutes');
+const postRoute = require('./routes/postRoutes');
+const groupRoute = require('./routes/groupRoutes');
+
+const PORT = 3000
+const app = express();
+
+
+
+require('dotenv').config()
+
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb+srv://biswasdiganta2001:hxuhYZ6LdwCapRjU@cluster0.vn82wq6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', () => {
+    console.log('Connected to database')
+})
+
+
+app.use("/user", userRoute)
+app.use("/post", postRoute)
+app.use("/group", groupRoute)
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+}
+)
